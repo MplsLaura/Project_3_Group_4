1c383df609c1c462d0c35d54dd8277bde6a56665
hamptonhughes <hannahcamilleri@Hannahs-MacBook-Pro-4.local>
2023-04-13 22:36:40 -0500

    added python app

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect
 from flask import Flask, jsonify
-#from flask_cors import CORS, cross_origin
+import json
+from flask_cors import CORS, cross_origin
 
 #Database setup
-engine = create_engine("sqlite:///Weather.db")
+engine = create_engine("sqlite:///Weather2.db")
 
 #reflect database and tables
 Base=automap_base()
@@ -19,7 +20,7 @@ inspector = inspect(engine)
 print(inspector.get_table_names())
 print(Base.classes.keys())
 #Save references to tables
-AnnualWinter = Base.classes.AnnualWeather
+AnnualWinter = Base.classes.Weather
 
 
 
@@ -37,6 +38,7 @@ def Home():
     )
 
 @app.route("/api/v1.0/AnnualizedWeather")
+@cross_origin(supports_credentials=True)  # to prevent CORS errors
 def Winter():
     #create session
     session = Session(engine)    
@@ -47,10 +49,14 @@ def Winter():
     session.close()
     #Create empy list, then a for loop with an empty dict.  Append data to dictionary
     winter_data =[]
-    for Season,AvgMaxTemp,AvgMinTemp,TotalPrecip,TotalSnowfall,Total_Days_6in_Bas,MaxRolling5Year,MinRolling5Year in data:
+    for Season,AvgMaxTemp,AvgMinTemp,TotalPrecip,TotalSnowfall,Total_Days_6in_Base,MaxRolling5Year,MinRolling5Year in data:
         winter_dict = {}
         winter_dict['Season'] = Season
-        winter_dict['Avg Max Temp'] = AvgMaxTemp
+        winter_dict['Avg Max Temp'] = float(AvgMaxTemp)
+        winter_dict['Avg Mix Temp'] = float(AvgMinTemp)
+        winter_dict['Total Precipitation'] = float(TotalPrecip)
+        winter_dict['TotalSnowfall'] = float(TotalSnowfall)
+        winter_dict['# of Days with 6 inch snowcover'] = int(Total_Days_6in_Base)
         winter_data.append(winter_dict)
     return jsonify(winter_data)
 
