fe708c36779651779dafc919379c6dcd899a8d3c
hamptonhughes <hannahcamilleri@Hannahs-MacBook-Pro-4.local>
2023-04-14 08:12:30 -0500

    Fixed one issue on app

--- app.py
+++ app.py
@@ -39,6 +39,31 @@ def Home():
         f"/api/v1.0/DailyWeather"
     )
 
+
+@app.route("/api/v1.0/AnnualizedWeather")
+@cross_origin(supports_credentials=True)  # to prevent CORS errors
+def Winter():
+    #create session
+    session = Session(engine)    
+    #query the data
+    data = session.query(AnnualWinter.Season,AnnualWinter.AvgMaxTemp,AnnualWinter.AvgMinTemp,\
+            AnnualWinter.TotalPrecip,AnnualWinter.TotalSnowfall,AnnualWinter.Total_Days_6in_Base,\
+            AnnualWinter.MaxRolling5Year,AnnualWinter.MinRolling5Year).all()
+    session.close()
+    #Create empy list, then a for loop with an empty dict.  Append data to dictionary
+    winter_data =[]
+    for Season,AvgMaxTemp,AvgMinTemp,TotalPrecip,TotalSnowfall,Total_Days_6in_Base,MaxRolling5Year,MinRolling5Year in data:
+        winter_dict = {}
+        winter_dict['Season'] = Season
+        winter_dict['Avg Max Temp'] = float(AvgMaxTemp)
+        winter_dict['Avg Mix Temp'] = float(AvgMinTemp)
+        winter_dict['Total Precipitation'] = float(TotalPrecip)
+        winter_dict['TotalSnowfall'] = float(TotalSnowfall)
+        winter_dict['# of Days with 6 inch snowcover'] = int(Total_Days_6in_Base)
+        winter_data.append(winter_dict)
+    return jsonify(winter_data)
+
+
 @app.route("/api/v1.0/DailyWeather")
 @cross_origin(supports_credentials=True)  # to prevent CORS errors
 def Daily():
