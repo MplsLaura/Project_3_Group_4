4f9864d996e4a7fcdfcfec6c85417d6f5e947e50
MplsLaura <lwmurray@gmail.com>
2023-04-19 08:43:38 -0500

    Final Commit

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,8 +1,18 @@
 
 
 // Use this link to get the GeoJSON data.
-let link = "http://192.168.1.131:5000/api/v1.0/DailyWeather";
-//  let link = "http://127.0.0.1:5000/api/v1.0/DailyWeather";
+//let link = "http://192.168.1.131:5000/api/v1.0/DailyWeather";
+let link = "http://127.0.0.1:5000/api/v1.0/DailyWeather";
+
+function dayOfYear (date) {
+    // the following code was pulled from: https://stackoverflow.com/questions/8619879/javascript-calculate-the-day-of-the-year-1-366
+    var start = new Date(date.getFullYear(), 0, 0);
+    var diff = (date - start) + ((start.getTimezoneOffset() - date.getTimezoneOffset()) * 60 * 1000);
+    var oneDay = 1000 * 60 * 60 * 24;
+    var day = Math.floor(diff / oneDay);
+    return  (day);
+
+}
     crossOrigin: null
 
 d3.json(link).then(function(data) {
@@ -10,13 +20,13 @@ d3.json(link).then(function(data) {
     let snowOut = [];
     // the following from https://www.techiedelight.com/initialize-array-with-range-from-0-to-n-javascript/
     let years = Array.from({length:200}, (item, index) => 1900 + index);
-    let foundEnd = Array.from({lenth:200}, (item, index) => false)
+
     var currYear = -1
 
     let isWinter = false;
     let daysNoSnow = 0;
-    //let dateOptions = { day: 'numeric', month: 'short'}
-    let dateOptions = {month: 'numeric'}
+    let dateOptions = { day: 'numeric', month: 'short'}
+  //  let dateOptions = {month: 'short'}
     for(let index=data.length-1; index > -1; index--) {
         const dateVal = new Date (data[index]['Date'])
         let foundEnd = 0
@@ -31,7 +41,7 @@ d3.json(link).then(function(data) {
             daysNoSnow = 0;
         }
         if (daysNoSnow == 1) { 
-            snowOut[currYear] = dateVal.toLocaleDateString("en-US", dateOptions);
+            snowOut[currYear] = dayOfYear(dateVal);
             console.log ("snow day " + snowOut[currYear])
         }
 //        console.log (data[index]['Date'] + " " + daysNoSnow + " " + snowCover);
@@ -40,7 +50,7 @@ d3.json(link).then(function(data) {
             isWinter = true;
             daysNoSnow = 0;
             currYear++;
-            snowIn[currYear] = dateVal.toLocaleDateString("en-US", dateOptions);
+            snowIn[currYear] = dayOfYear(dateVal);
             years[currYear] = (1900 + year);
             console.log ("winter of " + (1900 + currYear) + " starts " + snowIn[currYear]);
         } else if (year >= 0 && month < 8 && isWinter == true && daysNoSnow == 80){
@@ -49,82 +59,122 @@ d3.json(link).then(function(data) {
             console.log ("winter of " + (1900 + currYear) + " ends " + snowOut[currYear]);
         }
     }
+    let newYears = new Array(currYear  + 1).fill(366)
+
     console.log (snowIn)
     console.log (snowOut)
+    console.log (newYears)
 
 
     var trace1 = {
         x: years,
-        y: snowIn, 
-        fillcolor: "rgba(0,100,80,0.2)", 
-     //   fill: "tonexty",
+        y: snowIn,
         line: {color: "blue"}, 
         name: "First Snow", 
-
         showlegend: true,
-        type: "scatter",
-        yaxis: 'y'
+        type: "scatter"
+  
       };
       var trace2 = {
         x: years,
         y: snowOut, 
-    //    fill: "tonexty", 
+        fill: "tonexty", 
         fillcolor: "rgba(0,176,246,0.2)", 
-        line: {color: "red"}, 
+        line: {color: "green"}, 
         name: "Last Snow", 
         showlegend: true, 
-        yaxis: 'y2',
-        type: "scatter"
+        type: "scatter",
+        yaxis: 'y2'
       };
+      var trace3 = {
+        x: years,
+        y: newYears,
+        line: {color: "red"},
+        opacity: 0,
+        fillcolor: "rgba(0,176,246,0.2)",
+        fill: "tonexty",
+        name: "January 1",
+      }
 
 
-      var data = [trace1, trace2];
+      var data = [trace1, trace2, trace3];
       var layout = {
+        title: {
+        text:'<b>Snow Cover in the Twin Cities 1900 - 2023</b>',
+        font: {
+          family: 'Courier New, monospace',
+          size: 24
+        },
+        xref: 'paper',
+        x: 0.05,
+    },
         paper_bgcolor: "rgb(255,255,255)", 
         plot_bgcolor: "rgb(229,229,229)", 
         xaxis: {
+            title: {
+                text: '<b>Year</b>',
+                font: {
+                  family: 'Courier New, monospace',
+                  size: 18,
+                  color: '#7f7f7f'
+                }
+            },
           gridcolor: "rgb(255,255,255)", 
-          range: [1900, (1900 + currYear)], 
           showgrid: true, 
           showline: false, 
           showticklabels: true, 
           tickcolor: "rgb(127,127,127)", 
           ticks: "outside", 
-          zeroline: false
+  //        zeroline: false
         }, 
         yaxis: {
-          gridcolor: "rgb(255,255,255)", 
-          showgrid: true, 
-          showline: false,
-          overlaying: 'y2',
-          range: [16,9],
-          showgrid: true, 
-          showline: false, 
-          tickformat: '%b',
-          tickmode: "array",
-          tickvals:[1,2,3,4,5,6,7,8,9,10,11,12],
-          ticktext:['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'], 
-          showticklabels: true, 
-          tickangle: 45,
-          tickcolor: "rgb(127,127,127)", 
-          ticks: "outside",
-          zeroline: false
-        },
-        yaxis2: {
+            title: {
+                text: '<b>Month</b>',
+                font: {
+                  family: 'Courier New, monospace',
+                  size: 18,
+                  color: '#7f7f7f'
+                }
+            },
             gridcolor: "rgb(255,255,255)", 
             showgrid: true, 
+            showline: false,
+            range: [(365 + 182), 182],
+            showgrid: false, 
             showline: false, 
-            range:[5,-2],
-            side: 'right',
-            showticklabels: true, 
-            tickmode: "array",
-            tickangle: 45,
-            tickvals: 1,
-            tickcolor: "rgb(127,127,127)", 
-            ticks: "outside", 
-            zeroline: false
-          }
 
+        //    tickvals:[1, 32, 60, 91, 120, 151, 180, 210, 241, 271, 302, 332],
+//            ticktext:['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'], 
+            tickvals: [182, 213,244,274,305,335,(356+180)],
+         //   tickvals:[332,   302,   271,   241,   210,   180, 0],    
+            ticktext:['Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', ''],
+            showticklabels: true, 
+            tickangle: 45,
+            tickcolor: "rgb(127,127,127)", 
+            ticks: "outside",
+            zeroline: false
+        },
+
+        yaxis2: {
+            overlaying: 'y',
+            gridcolor: "rgb(255,255,255)", 
+            showgrid: true, 
+            showline: false,
+            range: [182, -182],
+            showgrid: false, 
+            showline: false, 
+ //           tickvals: [180, 151,120,91,60,32,1],
+            tickvals:[-182, 1, 32, 60, 91, 121, 152],
+            ticktext:['', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'], 
+//            ticktext:['', 'May', 'Apr', 'Mar', 'Feb', 'Jan'],
+            showticklabels: true, 
+            tickangle: 45,
+            tickcolor: "rgb(127,127,127)", 
+            ticks: "outside",
+ //           side: 'right',
+             zeroline: false
+        }
+        
     }
     chartDiv = document.getElementById('layout')
     Plotly.newPlot(chartDiv, data, layout);
